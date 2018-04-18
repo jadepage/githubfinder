b469abc34d15cae81877f5a9d9a2758aca2e5cf7
Jade Page <jadepage@gmail.com>
2018-04-18 14:51:17 +0200

    added project

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+  constructor() {
+    this.client_id = 'aef8f89472fa006bfdcd';
+    this.client_secret = '3f89c7321df6f4618e23436e7b052e5cbb9c977e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
